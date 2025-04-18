592fd420680b81eaf71f7d431fd7332971ac1885
philyas <lephi@gmx.net>
2025-04-19 01:13:56 +0200

    add product and about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, ProductsComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
