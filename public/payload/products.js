ac0b703772b5dbd33e86a83c24e9542e36d1ec24
philyas <lephi@gmx.net>
2025-04-20 00:53:44 +0200

    add products payload json

---
+++ public/payload/products.js
@@ -0,0 +1,324 @@
+export const products = [
+    {
+      brand: "Somero",
+      products: [
+        {
+          name: "S-485 Laser Screed",
+          image: "images/hersteller/somero/somero1.jpg",
+          models: [
+            {
+              code: "S485-GX390",
+              engine: "Honda GX390",
+              rotor_diameter_cm: 90,
+              blade_count: 4,
+              output_speed_rpm: 150,
+              power_hp: 13,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 120
+            }
+          ]
+        },
+        {
+          name: "S-22EZ Laser Screed",
+          image: "images/hersteller/somero/somero2.jpg",
+          models: [
+            {
+              code: "S22EZ-KD1000",
+              engine: "Kubota D1105",
+              rotor_diameter_cm: 120,
+              blade_count: 5,
+              output_speed_rpm: 130,
+              power_hp: 24.8,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 1450
+            }
+          ]
+        },
+        {
+          name: "CopperHead XD 3.0",
+          image: "images/hersteller/somero/somero3.jpg",
+          models: [
+            {
+              code: "CHXD3-H390",
+              engine: "Honda GX390",
+              rotor_diameter_cm: 80,
+              blade_count: 4,
+              output_speed_rpm: 140,
+              power_hp: 13,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 100
+            }
+          ]
+        },
+        {
+          name: "S-158C Laser Screed",
+          image: "images/hersteller/somero/somero4.jpg",
+          models: [
+            {
+              code: "S158C-D902",
+              engine: "Kubota D902",
+              rotor_diameter_cm: 105,
+              blade_count: 5,
+              output_speed_rpm: 135,
+              power_hp: 22,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 1300
+            }
+          ]
+        },
+        {
+          name: "Mini Screed C",
+          image: "images/hersteller/somero/somero5.jpg",
+          models: [
+            {
+              code: "MSC-G390",
+              engine: "Honda GX390",
+              rotor_diameter_cm: 85,
+              blade_count: 4,
+              output_speed_rpm: 150,
+              power_hp: 13,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 110
+            }
+          ]
+        },
+        {
+          name: "S-10A Laser Screed",
+          image: "images/hersteller/somero/somero2.jpg",
+          models: [
+            {
+              code: "S10A-K1005",
+              engine: "Kubota D1105",
+              rotor_diameter_cm: 100,
+              blade_count: 5,
+              output_speed_rpm: 145,
+              power_hp: 25,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 1400
+            }
+          ]
+        }
+      ]
+    },
+    {
+      brand: "Barikell",
+      products: [
+        {
+          name: "BT900 Ride-On Trowel",
+          image: "images/hersteller/barikell/barikell1.jpg",
+          models: [
+            {
+              code: "BT900-H",
+              engine: "Honda GX690",
+              rotor_diameter_cm: 100,
+              blade_count: 4,
+              output_speed_rpm: 140,
+              power_hp: 22,
+              start: "Electric",
+              fuel_type: "Gasoline",
+              weight_kg: 330
+            }
+          ]
+        },
+        {
+          name: "BT1200 Twin Rotor",
+          image: "images/hersteller/barikell/barikell2.jpg",
+          models: [
+            {
+              code: "BT1200-KD",
+              engine: "Kohler Diesel",
+              rotor_diameter_cm: 120,
+              blade_count: 5,
+              output_speed_rpm: 135,
+              power_hp: 25,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 370
+            }
+          ]
+        },
+        {
+          name: "BT75 Walk-Behind",
+          image: "images/hersteller/barikell/barikell3.jpg",
+          models: [
+            {
+              code: "BT75-GX270",
+              engine: "Honda GX270",
+              rotor_diameter_cm: 75,
+              blade_count: 4,
+              output_speed_rpm: 140,
+              power_hp: 9,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 95
+            }
+          ]
+        },
+        {
+          name: "BT60 Walk-Behind",
+          image: "images/hersteller/barikell/barikell4.jpg",
+          models: [
+            {
+              code: "BT60-GX200",
+              engine: "Honda GX200",
+              rotor_diameter_cm: 60,
+              blade_count: 4,
+              output_speed_rpm: 135,
+              power_hp: 6.5,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 80
+            }
+          ]
+        },
+        {
+          name: "BT100 Dual Trowel",
+          image: "images/hersteller/barikell/barikell1.jpg",
+          models: [
+            {
+              code: "BT100-KH",
+              engine: "Kohler CH730",
+              rotor_diameter_cm: 100,
+              blade_count: 5,
+              output_speed_rpm: 150,
+              power_hp: 23,
+              start: "Electric",
+              fuel_type: "Gasoline",
+              weight_kg: 360
+            }
+          ]
+        },
+        {
+          name: "BT Compact Ride-On",
+          image: "images/hersteller/barikell/barikell2.jpg",
+          models: [
+            {
+              code: "BTC-HG390",
+              engine: "Honda GX390",
+              rotor_diameter_cm: 90,
+              blade_count: 4,
+              output_speed_rpm: 140,
+              power_hp: 13,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 280
+            }
+          ]
+        }
+      ]
+    },
+    {
+      brand: "Husqvarna",
+      products: [
+        {
+          name: "CRT36 Ride-On Trowel",
+          image: "images/hersteller/husqvarna/husqvarna1.webp",
+          models: [
+            {
+              code: "CRT36-GX690",
+              engine: "Honda GX690",
+              rotor_diameter_cm: 92,
+              blade_count: 5,
+              output_speed_rpm: 160,
+              power_hp: 22,
+              start: "Electric",
+              fuel_type: "Gasoline",
+              weight_kg: 315
+            }
+          ]
+        },
+        {
+          name: "CRT48 Power Trowel",
+          image: "images/hersteller/husqvarna/husqvarna2.webp",
+          models: [
+            {
+              code: "CRT48-KD750",
+              engine: "Kohler Diesel 7500",
+              rotor_diameter_cm: 120,
+              blade_count: 6,
+              output_speed_rpm: 140,
+              power_hp: 25,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 400
+            }
+          ]
+        },
+        {
+          name: "CT48 Walk-Behind",
+          image: "images/hersteller/husqvarna/husqvarna3.webp",
+          models: [
+            {
+              code: "CT48-GX390",
+              engine: "Honda GX390",
+              rotor_diameter_cm: 122,
+              blade_count: 4,
+              output_speed_rpm: 135,
+              power_hp: 13,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 115
+            }
+          ]
+        },
+        {
+          name: "CT36 Walk-Behind",
+          image: "images/hersteller/husqvarna/husqvarna2.webp",
+          models: [
+            {
+              code: "CT36-GX270",
+              engine: "Honda GX270",
+              rotor_diameter_cm: 90,
+              blade_count: 4,
+              output_speed_rpm: 140,
+              power_hp: 9,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 95
+            }
+          ]
+        },
+        {
+          name: "BG375 Trowel",
+          image: "images/hersteller/husqvarna/husqvarna1.webp",
+          models: [
+            {
+              code: "BG375-GX200",
+              engine: "Honda GX200",
+              rotor_diameter_cm: 60,
+              blade_count: 4,
+              output_speed_rpm: 135,
+              power_hp: 6.5,
+              start: "Manual",
+              fuel_type: "Gasoline",
+              weight_kg: 85
+            }
+          ]
+        },
+        {
+          name: "CRT60 Hydraulic Trowel",
+          image: "images/hersteller/husqvarna/husqvarna3.webp",
+          models: [
+            {
+              code: "CRT60-HYD-KD1000",
+              engine: "Kubota Diesel",
+              rotor_diameter_cm: 125,
+              blade_count: 6,
+              output_speed_rpm: 150,
+              power_hp: 30,
+              start: "Electric",
+              fuel_type: "Diesel",
+              weight_kg: 450
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
