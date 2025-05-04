6432cd929263adebc8c27daba9af0055c13c97b7
FiratTasyurdu <131069064+JNZN93@users.noreply.github.com>
2025-05-04 21:44:35 +0200

    translate welding page

--- src/app/schweisstechnik/schweisstechnik.component.ts
+++ src/app/schweisstechnik/schweisstechnik.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ContactFormularComponent } from '../contact-formular/contact-formular.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-schweisstechnik',
-  imports: [RouterModule, ContactFormularComponent],
+  imports: [RouterModule, ContactFormularComponent, TranslateModule],
   templateUrl: './schweisstechnik.component.html',
   styleUrl: './schweisstechnik.component.scss'
 })
